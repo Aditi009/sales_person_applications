c529a3a8562dd629f34daab7d2e9132fbb4f58ec
Aditi Gupta <aditissdn@gmail.com>
2023-02-17 10:51:44 +0530

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,361 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="{{ asset('assets/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container section-one">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="form-group d-flex p-1">
+                    <label class="btn btn-title">Date:</label>
+                    <input type="text" class="ml-5 form-control">
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="row d-flex">
+                    <label class="btn btn-title">Division:</label>
+                    <div class="checkbox-container" style="">
+                        <input type="checkbox">
+                        <span>US</span>
+                    </div>
+                    <div class="checkbox-container" style="">
+                        <input type="checkbox">
+                        <span>UK</span>
+                    </div>
+                    <div class="checkbox-container" style="">
+                        <input type="checkbox">
+                        <span>AU</span>
+                    </div>
+                    <div class="checkbox-container" style="">
+                        <input type="checkbox">
+                        <span>NZ</span>
+                    </div>
+                    <div class="checkbox-container" style="">
+                        <input type="checkbox">
+                        <span>DD</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-7">
+                <div class="form-group d-flex p-1">
+                    <label class="btn btn-title">Sale Agent Number:</label>
+                    <input type="text" class="ml-5 form-control">
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="form-group d-flex p-1">
+                    <label class="btn btn-title">Sale Agent Person:</label>
+                    <input type="text" class="ml-5 form-control">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="form-group d-flex p-1">
+                    <label class="btn btn-title">Sale Agent Number:</label>
+                    <input type="text" class="ml-5 form-control">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-group d-flex p-1">
+                    <label class="btn btn-title">Sale Agent Position:</label>
+                    <input type="text" class="ml-5 form-control">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-group d-flex p-1">
+                    <label class="btn btn-title">Sale Agent Position:</label>
+                    <input type="text" class="ml-5 form-control">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container section-two">
+        <h3 class="cus-h3">Type</h3>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>DD</span>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>DD</span>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>DD</span>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>DD</span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="container section-two">
+        <h3 class="cus-h3">Service & Products</h3>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>DD</span>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>DD</span>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>DD</span>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>DD</span>
+                </div>
+            </div>
+
+        </div>
+        <div class="row mt-2">
+            <div class="col-md-3">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>DD</span>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>DD</span>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>DD</span>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>DD</span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container section-three">
+        <h3 class="cus-h3">Sale Amount</h3>
+        <div class="row">
+            <div class="col-md-2">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>$500</span>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>$1000</span>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>$2000</span>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>$5000</span>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>$10000</span>
+                </div>
+            </div>
+
+        </div>
+        <div class="row mt-2">
+            <div class="col-md-2">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>DD</span>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>DD</span>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>DD</span>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>Other</span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container section-four">
+        <div class="row">
+            <div class="col-md-3">
+                <h3 class="bold">Package Detail</h3>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group d-flex p-15">
+                    <label class="btn btn-title">Bonus 1:</label>
+                    <input type="text" class="ml-5 form-control">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group d-flex p-15">
+                    <label class="btn btn-title">Bonus 2:</label>
+                    <input type="text" class="ml-5 form-control">
+                </div>
+            </div>
+            <div class="col-md-3 p-15">
+                <div class="checkbox-container-3 p-5" style="">
+                    <input type="checkbox">
+                    <span>Not Applicable</span>
+                </div>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                <div class="form-group d-flex">
+                    <label class="btn btn-title">Package/s:</label>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>$500</span>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>$1000</span>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>$2000</span>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <label class="btn btn-title">Package Total
+                    Amount: </label>
+            </div>
+            <div class="col-md-2">
+                <input type="text" class="ml-5 form-control">
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="container section-five">
+        <div class="row">
+            <div class="col-md-4">
+                <h3 class="bold">Sale Details - Applicant 1</h3>
+            </div>
+            <div class="col-md-2">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>Joint</span>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>Single</span>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <label class="btn btn-title">Percentage Of Owner </label>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                <div class="form-group d-flex">
+                    <label class="btn btn-title">Package/s:</label>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>$500</span>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>$1000</span>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="checkbox-container" style="">
+                    <input type="checkbox">
+                    <span>$2000</span>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <label class="btn btn-title">Package Total
+                    Amount: </label>
+            </div>
+            <div class="col-md-2">
+                <input type="text" class="ml-5 form-control">
+            </div>
+
+        </div>
+
+    </div>
+    <script></script>
+</body>
+
+</html>
